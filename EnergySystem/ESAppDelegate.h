af1ec376fd6b44cee3b631e5599ae2d54a1f0841
tseg <tseg@tsegdeMacBook-Pro.local>
2014-08-25 15:58:41 +0800

    Initial Commit

---
+++ EnergySystem/ESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESAppDelegate.h
+//  EnergySystem
+//
+//  Created by tseg on 14-8-25.
+//  Copyright (c) 2014年 tseg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EnergySystem/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EnergySystem
+//
+//  Created by tseg on 14-8-25.
+//  Copyright (c) 2014年 tseg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ESAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ESAppDelegate class]));
+    }
+}
